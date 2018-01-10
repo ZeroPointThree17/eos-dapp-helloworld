06e10a62966880f2a8d8a817c5a73f90a48d04cc
zeropointthree17 <32686777+ZeroPointThree17@users.noreply.github.com>
2018-01-09 19:23:23 -0600

    Initial Commit

---
+++ mydapp.cpp
@@ -0,0 +1,26 @@
+/**
+ *  @file
+ *  @copyright defined in eos/LICENSE.txt
+ */
+#include "mydapp.hpp"
+#include "mydapp.gen.hpp"
+#include <eoslib/db.hpp>
+
+extern "C" {
+   void init()  {
+
+   }
+
+   void apply( uint64_t code, uint64_t action ) {
+      if( code == N(helloworld) ) {
+         if( action == N(change) ) {
+        	auto kv1 = eosio::current_message<key_value>();
+            bytes b = eosio::raw::pack(kv1.lastmod);
+            uint32_t err = store_str( N(helloworld), N(change), (char *)kv1.key.get_data(), kv1.key.get_size(), (char*)b.data, b.len);
+
+         } else {
+            assert(0, "unknown message");
+         }
+      }
+   }
+}
---
+++ mydapp.hpp
@@ -0,0 +1,10 @@
+#include <eoslib/string.hpp>
+#include <eoslib/eos.hpp>
+
+/* @abi action change
+ * @abi table
+*/
+struct key_value {
+   eosio::string key;
+   time lastmod;
+};
