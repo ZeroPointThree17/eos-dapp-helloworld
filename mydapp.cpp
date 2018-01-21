71481535d80739788fa584c4420754e18a5f75c9
ZeroPointThree17 <32686777+ZeroPointThree17@users.noreply.github.com>
2018-01-20 18:03:48 -0600

    Update mydapp.cpp

--- mydapp.cpp
+++ mydapp.cpp
@@ -14,7 +14,8 @@ extern "C" {
    void apply( uint64_t code, uint64_t action ) {
       if( code == N(helloworld) ) {
          if( action == N(change) ) {
-        	   auto kv1 = eosio::current_message<key_value>();
+            
+            auto kv1 = eosio::current_message<key_value>();
             bytes b = eosio::raw::pack(kv1.value);
             uint32_t err = store_str( N(helloworld), N(keyvalue), (char *)kv1.key.get_data(), kv1.key.get_size(), (char*)b.data, b.len);
 
